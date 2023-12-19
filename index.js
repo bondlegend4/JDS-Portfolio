53aebe26f1044e373e5f7e26d951d97cc6f18ff9
bondlegend0110 <jose_sandoval@brown.edu>
2023-12-19 18:56:48 -0500

    fixed the sticky situation i am using my school github account until i use a vm to login to the admin for this project

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ var sticky = navbar.offsetTop; // Get the offset position of the navbar
 
 window.addEventListener('scroll', function() {
     var navbar = document.getElementById('navbar');
-    if (window.scrollY >= sticky) {
-      navbar.classList.add('sticky-top');
+    if (window.scrollY==0){
+      navbar.classList.remove('at-top');
+      navbar.classList.remove('moving');
+      navbar.classList.add('at-bottom');
+    }
+    else if(window.scrollY >= sticky) {
+      navbar.classList.add('at-top');
+      navbar.classList.remove('moving');
       navbar.classList.remove('at-bottom');
     } else {
-      navbar.classList.remove('sticky-top');
-      navbar.classList.add('at-bottom');
+      navbar.classList.remove('at-top');
+      navbar.classList.add('moving');
+      navbar.classList.remove('at-bottom');
     }
   });
